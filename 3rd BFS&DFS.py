90f7a6090b7885371a17a9f794b48527d1253330
Divya Somineni <divyasomineni2908@gmail.com>
2026-01-08 15:52:25 +0530

    Add files via upload

---
+++ 3rd BFS&DFS.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+graph = {
+    1: [2, 3],
+    2: [5, 6],
+    3: [4],
+    4: [7, 8],
+    5: [],
+    6: [],
+    7: [],
+    8: []
+}
+
+def bfs(start):
+    visited = set()
+    queue = deque([start])
+    print("BFS:", end=" ")
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(graph[node])
+
+def dfs(start):
+    visited = set()
+    print("DFS:", end=" ")
+    def dfs_util(node):
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            for n in graph[node]:
+                dfs_util(n)
+    dfs_util(start)
+
+bfs(1)
+print()
+dfs(1)
